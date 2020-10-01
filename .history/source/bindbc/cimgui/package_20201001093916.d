c4a6c3fb2f20dc6a4518fec61ea96645a5b45573
Hipreme <msnmancini@hotmail.com>
2020-10-01 10:02:18 -0300

    Added licenses

---
+++ .history/source/bindbc/cimgui/package_20201001093916.d
@@ -0,0 +1,5 @@
+module bindbc.cimgui;
+
+public import bindbc.cimgui.funcs;
+public import bindbc.cimgui.cimguiload;
+public import bindbc.cimgui.types;
--- .history/source/bindbc/cimgui/types_20201001093819.d
+++ .history/source/bindbc/cimgui/types_20201001093819.d
@@ -1,9 +1,3 @@
-
-//          Copyright Marcelo S. N. Mancini(Hipreme) 2020.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
 module bindbc.cimgui.types;
 import core.stdc.config;
 import core.stdc.stdarg: va_list;
