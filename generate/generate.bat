ed9f6619fc0029492422fa9465b691f184502e82
AntonC9018 <anton.curm@gmail.com>
2021-09-08 14:42:46 +0300

    debugging

--- generate/generate.bat
+++ generate/generate.bat
@@ -1,2 +1,4 @@
+pushd .
 cd ../bindbc-generator
-dub -- --load-all --recompile --plugin-args cimgui-overloads="[../cimgui ../source/bindbc/cimgui d-conv]"
+dub -- --recompile --load-all --file="../cimgui/cimgui.h" --plugin-args cimgui-overloads="[../cimgui ../source/bindbc/cimgui2 d-conv]" --dpp-path="E:\Coding\D\dpp\bin\d++.exe" --presets="cimgui"
+popd
